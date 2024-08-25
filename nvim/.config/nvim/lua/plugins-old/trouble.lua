5b0330924e336642bdd3a90c43f555751219ef2c
Dumitru-Daniel Mititelu <mttl_daniel@yahoo.com>
2024-08-25 21:22:32 +0300

    delete nvim config

--- nvim/.config/nvim/lua/plugins-old/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        vim.keymap.set("n", "<leader>t", "<cmd>TroubleToggle<cr>",
-            { silent = true, noremap = true }
-        )
-    end
-}
